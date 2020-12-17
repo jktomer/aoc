2cb497b3c3ab038edabb30c25e4e66be56fc48a9
Jonathan Klabunde Tomer <jonathantomer@gmail.com>
2020-12-17 00:59:20 -0800

    day 17, but not gross

--- 2020/day17.pl
+++ 2020/day17.pl
@@ -3,96 +3,38 @@
 use strict;
 use Clone 'clone';
 
-my %state;
+my (%state3, %state4);
 while (<>) {
     chomp;
     my @row = split(//);
-    $state{0}{$.-1}{$_} = 1 for grep {$row[$_] eq '#'} (0..$#row);
+    $state3{"0," . ($.-1) . ",$_"} = 1 for grep {$row[$_] eq '#'} (0..$#row);
+    $state4{"0,0," . ($.-1) . ",$_"} = 1 for grep {$row[$_] eq '#'} (0..$#row);
 }
 
-sub step3($) {
-    my $in = shift;
+sub step($%) {
+    my ($dims, %in) = @_;
     my %out;
     my %neighbors;
-    while (my ($z, $plane) = each %$in) {
-        while (my ($y, $row) = each %$plane) {
-            while (my ($x, $cell) = each %$row) {
-                my $neighbors = -$cell;
-                for my $zz ($z - 1 .. $z + 1) {
-                    for my $yy ($y - 1 .. $y + 1) {
-                        for my $xx ($x - 1 .. $x + 1) {
-                            next if $xx == $x and $yy == $y and $zz == $z;
-                            $neighbors{$zz}{$yy}{$xx}++;
-                        }
-                    }
-                }
-            }
+    for my $coords (keys %in) {
+        my @coords = split(/,/, $coords);
+        my @neighborCoords = ("");
+        for my $dim (0..$dims - 1)
+        {
+            @neighborCoords = map { my $prefix = length($_) ? "$_," : '' ; map { "$prefix$_" } ($coords[$dim]-1..$coords[$dim]+1) } @neighborCoords;
         }
+        $neighbors{$_}++ for @neighborCoords;
+        $neighbors{$coords}--;
     }
-    while (my ($z, $plane) = each %neighbors) {
-        while (my ($y, $row) = each %$plane) {
-            while (my ($x, $nbrs) = each %$row) {
-                $out{$z}{$y}{$x} = 1 if $nbrs == 3 or ($nbrs == 2 and $in->{$z}{$y}{$x});
-            }
-        }
+    while (my ($coords, $nbrs) = each %neighbors) {
+        $out{$coords} = 1 if $nbrs == 3 or ($nbrs == 2 and $in{$coords});
     }
-    return \%out;
+    return %out;
 }
 
-sub step4($) {
-    my $in = shift;
-    my %out;
-    my %neighbors;
-    while (my ($w, $cube) = each %$in)
-    {
-        while (my ($z, $plane) = each %$cube)
-        {
-            while (my ($y, $row) = each %$plane)
-            {
-                while (my ($x, $cell) = each %$row)
-                {
-                    my $neighbors = -$cell;
-                    for my $ww ($w - 1 .. $w + 1)
-                    {
-                        for my $zz ($z - 1 .. $z + 1)
-                        {
-                            for my $yy ($y - 1 .. $y + 1)
-                            {
-                                for my $xx ($x - 1 .. $x + 1)
-                                {
-                                    next if $xx == $x and $yy == $y and $zz == $z and $ww == $w;
-                                    $neighbors{$ww}{$zz}{$yy}{$xx}++;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    while (my ($w, $cube) = each %neighbors)
-    {
-        while (my ($z, $plane) = each %$cube)
-        {
-            while (my ($y, $row) = each %$plane)
-            {
-                while (my ($x, $nbrs) = each %$row)
-                {
-                    $out{$w}{$z}{$y}{$x} = 1 if $nbrs == 3 or ($nbrs == 2 and $in->{$w}{$z}{$y}{$x});
-                }
-            }
-        }
-    }
-    return \%out;
-}
-
-my $state4 = { 0 => clone(\%state) };
-
-my $state3 = \%state;
 for my $i (1..6) {
-    $state3 = step3($state3);
-    $state4 = step4($state4);
+    %state3 = step(3, %state3);
+    %state4 = step(4, %state4);
 }
-my $on3 = map { map { values %$_ } values %$_ } values %$state3;
-my $on4 = map { map { map { values %$_ } values %$_ } values %$_ } values %$state4;
+my $on3 = values %state3;
+my $on4 = values %state4;
 print "$on3 $on4\n";
