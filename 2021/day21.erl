3c3c7b2453e1bf155030c602db89fb90db121692
Jonathan Klabunde Tomer <jonathantomer@gmail.com>
2021-12-21 23:42:08 -0800

    2021 day 21 part 2

--- 2021/day21.erl
+++ 2021/day21.erl
@@ -4,7 +4,7 @@
 -mode(compile).
 
 main(_) ->
-    {ok, File} = file:open("day21e.txt", [read]),
+    {ok, File} = file:open("day21.txt", [read]),
     {P1, P2} = read_input(File),
     {LoseScore, Rolls} = play_dirac_deterministic(P1, P2),
     NWins = play_dirac_nondeterministic(P1, P2),
@@ -22,33 +22,42 @@ play_dirac_deterministic(P1, S1, P2, S2, Rolls, Roll) ->
     S1x = S1 + P1x,
     play_dirac_deterministic(P2, S2, P1x, S1x, Rolls + 3, NextRoll).
 
-play_dirac_nondeterministic(P1, P2) when is_integer(P1) ->
-    element(1, play_dirac_nondeterministic_impl({P1, 0, P2, 0, 2}, #{})).
+play_dirac_nondeterministic(P1, P2) ->
+    {A, B, _} = play_dirac_nondeterministic_impl({{P1, P2}, {0, 0}, 1}, #{}),
+    max(A, B).
 
-play_dirac_nondeterministic_impl(Key = {P1, S1, P2, S2, Who}, Cache) ->
+play_dirac_nondeterministic_impl(Key = {{P1, P2}, {S1, S2}, Who}, Cache) ->
     case Cache of
-        #{Key := Val} ->
-            {Val, Cache};
+        #{Key := {A, B}} ->
+            {A, B, Cache};
+        _ when S1 >= 21 ->
+            {1, 0, Cache#{{P1, S1, P2, S2, Who} => {1, 0}}};
         _ when S2 >= 21 ->
-            {Who rem 2, Cache#{{P1, S1, P2, S2, Who} => 1}};
+            {0, 1, Cache#{{P1, S1, P2, S2, Who} => {0, 1}}};
         _ ->
-            {_, Res, Cache1} = lists:foldl(
-              fun play_dirac_nondeterministic_step/2,
-              {Key, 0, Cache},
+            {AWin, BWin, Cache1} = lists:foldl(
+              fun(Roll, {AAcc, BAcc, CAcc}) ->
+                      {A, B, C} = play_dirac_nondeterministic_step(Key, Roll, CAcc),
+                      {AAcc+A, BAcc+B, C}
+              end,
+              {0, 0, Cache},
               [
                A+B+C ||
                   A <- [1, 2, 3],
                   B <- [1, 2, 3],
                   C <- [1, 2, 3]
               ]),
-            {Res, Cache1#{Key => Res}}
+            {AWin, BWin, Cache1#{Key => {AWin, BWin}}}
     end.
 
-play_dirac_nondeterministic_step(Roll, {Key = {P1, S1, P2, S2, Who}, Acc, Cache}) ->
-    P1x = (P1 + Roll - 1) rem 10 + 1,
-    S1x = S1 + P1x,
-    {Res, Cache1} = play_dirac_nondeterministic_impl({P2, S2, P1x, S1x, 3 - Who}, Cache),
-    {Key, Acc+Res, Cache1}.
+play_dirac_nondeterministic_step({Pos, Score, Who}, Roll, Cache) ->
+    Pos1 = (element(Who, Pos) + Roll - 1) rem 10 + 1,
+    Score1 = element(Who, Score) + Pos1,
+    play_dirac_nondeterministic_impl(
+      {setelement(Who, Pos, Pos1),
+       setelement(Who, Score, Score1),
+       3 - Who},
+      Cache).
 
 read_input(File) ->
     {ok, "Player 1 starting position: " ++ [P1 | "\n"]} = file:read_line(File),
