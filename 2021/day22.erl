10067c88efd27e8e19af6d9fd451f89d5532850f
Jonathan Klabunde Tomer <jonathantomer@gmail.com>
2021-12-21 22:10:22 -0800

    2021 day 22 part 2

--- 2021/day22.erl
+++ 2021/day22.erl
@@ -7,7 +7,11 @@ main(_) ->
     {ok, File} = file:open("day22.txt", [read]),
     Steps = read_input(File),
     Init = do_cubes(lists:filtermap(fun restrict/1, Steps)),
-    io:format("~p ~p~n", [sets:size(Init), 0]).
+    Final = do_cubes(Steps),
+    io:format("~p ~p~n", [active_size(Init), active_size(Final)]).
+
+active_size(Areas) ->
+    lists:sum([(XH+1-XL)*(YH+1-YL)*(ZH+1-ZL) || {XL, XH, YL, YH, ZL, ZH} <- Areas]).
 
 restrict({_, {XL, XH, YL, YH, ZL, ZH}}) when
       XL > 50 orelse YL > 50 orelse ZL > 50 orelse
@@ -17,24 +21,34 @@ restrict({T, Ranges}) ->
     {true, {T, list_to_tuple([min(50, max(-50, V)) || V <- tuple_to_list(Ranges)])}}.
 
 do_cubes(Steps) ->
-    lists:foldl(fun do_cubes_impl/2, sets:new(), Steps).
+    lists:foldl(fun do_cubes_impl/2, [], Steps).
 
-do_cubes_impl({on, Ranges}, State) ->
-    lists:foldl(
-      fun sets:add_element/2,
-      State,
-      all_points(Ranges));
-do_cubes_impl({off, Ranges}, State) ->
-    lists:foldl(
-      fun sets:del_element/2,
-      State,
-      all_points(Ranges)).
+do_cubes_impl({State, Target}, Active) ->
+    Nonoverlapping = lists:append([split(Target, Area) || Area <- Active]),
+    do_cubes_impl(State, Target, Nonoverlapping).
 
-all_points({XL, XH, YL, YH, ZL, ZH}) ->
-      [{X, Y, Z} ||
-          X <- lists:seq(XL, XH),
-          Y <- lists:seq(YL, YH),
-          Z <- lists:seq(ZL, ZH)].
+do_cubes_impl(off, _, Active) ->
+    Active;
+do_cubes_impl(on, Target, Active) ->
+    [Target | Active].
+
+split({SXL, SXH, SYL, SYH, SZL, SZH}, {EXL, EXH, EYL, EYH, EZL, EZH}) ->
+    OXL = max(EXL, SXL),
+    OXH = min(EXH, SXH),
+    OYL = max(EYL, SYL),
+    OYH = min(EYH, SYH),
+    lists:filter(fun nonempty/1,
+      [
+       {EXL, min(EXH, SXL-1), EYL, EYH, EZL, EZH},
+       {max(EXL, SXH + 1), EXH, EYL, EYH, EZL, EZH},
+       {OXL, OXH, EYL, min(EYH, SYL-1), EZL, EZH},
+       {OXL, OXH, max(EYL, SYH+1), EYH, EZL, EZH},
+       {OXL, OXH, OYL, OYH, EZL, min(EZH, SZL-1)},
+       {OXL, OXH, OYL, OYH, max(EZL, SZH+1), EZH}
+      ]).
+
+nonempty({XL, XH, YL, YH, ZL, ZH}) ->
+    XL =< XH andalso YL =< YH andalso ZL =< ZH.
 
 read_input(File) ->
     lists:reverse(read_input(File, [])).
